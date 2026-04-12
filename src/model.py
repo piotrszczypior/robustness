37e4bab17a181d1e7762809bec5af5692361d0fb
Piotr Szczypior <piotrszczypior.mail@gmail.com>
2026-04-12 20:52:36 +0200

    update models list

--- src/model.py
+++ src/model.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 from typing import Callable, Tuple
 
 import torchvision.models as models
@@ -16,8 +17,12 @@ class _ModelFactory:
     _REGISTRY = {
         "resnet50": models.ResNet50_Weights.IMAGENET1K_V2,
         "resnet152": models.ResNet152_Weights.IMAGENET1K_V1,
+        "wide_resnet50_2": models.Wide_ResNet50_2_Weights.IMAGENET1K_V2,
+        "densenet121": models.DenseNet121_Weights.IMAGENET1K_V1, 
+        "mobilenet_v3_large": models.MobileNet_V3_Large_Weights.IMAGENET1K_V2,
         "efficientnet_b4": models.EfficientNet_B4_Weights.IMAGENET1K_V1,
         "vit_b_16": models.ViT_B_16_Weights.IMAGENET1K_V1,
+        "swin_b": models.Swin_B_Weights.IMAGENET1K_V1,
         "convnext_base": models.ConvNeXt_Base_Weights.IMAGENET1K_V1,
     }
 
